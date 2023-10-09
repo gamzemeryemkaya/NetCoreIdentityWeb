ef6f0437c6a49d878b97a65c4ae2a77d875efc2d
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-10-09 23:24:22 +0300

    üye sayfası( profil giriş) sayfası tamamlandi

--- NetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ NetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 using NetCoreIdentityApp.Web.Models;
+using NetCoreIdentityApp.Web.ViewModels;
 
 namespace NetCoreIdentityApp.Web.Controllers
 {
@@ -10,18 +11,41 @@ namespace NetCoreIdentityApp.Web.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
         // Dependency Injection ile SignInManager'ı alır
-        public MemberController(SignInManager<AppUser> signInManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         // Ana sayfa
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // Kullanıcı bilgilerini görüntülemek için mevcut kullanıcıyı alır
+
+            // User.Identity!.Name!, mevcut kimlik altında kullanıcı adını alır ve null olmadığını doğrular
+            var currentUser = (await _userManager.FindByNameAsync(User.Identity!.Name!))!;
+
+            // UserViewModel sınıfını kullanarak kullanıcı bilgilerini görüntülemek için bir görünüm modeli oluşturur
+
+            var userViewModel = new UserViewModel
+            {
+                // Kullanıcının e-posta adresini alır ve UserViewModel'e ekler
+                Email = currentUser.Email,
+
+                // Kullanıcının kullanıcı adını alır ve UserViewModel'e ekler
+                UserName = currentUser.UserName,
+
+                // Kullanıcının telefon numarasını alır ve UserViewModel'e ekler
+                PhoneNumber = currentUser.PhoneNumber,
+            };
+
+            // Oluşturulan UserViewModel'i kullanarak bir görünüm döndürür
+            return View(userViewModel);
         }
+
 
         // Çıkış işlemi için kullanılan action
         public async Task Logout()
