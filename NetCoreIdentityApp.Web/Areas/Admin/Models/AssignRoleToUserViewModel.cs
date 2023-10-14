4469afcca03f67e71b18b677eb5c74d7f4bc2646
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-10-14 10:55:16 +0300

    rol atama işlemi tamamlandi

---
+++ NetCoreIdentityApp.Web/Areas/Admin/Models/AssignRoleToUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCoreIdentityApp.Web.Areas.Admin.Models
+{
+    //rol atama için
+    public class AssignRoleToUserViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        //bu role sahip mi değil mi -var mı yok mu
+        public bool Exist { get; set; }
+    }
+}
